b696d6979ac60e09ce2f84917940a186c986a713
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-25 22:29:16 +0300

    feat(finance): add Transaction entity and TransactionType enum

---
+++ MiniERP.Domain/Enums/TransactionType.cs
@@ -0,0 +1,14 @@
+﻿namespace MiniERP.Domain.Enums
+{
+    public enum TransactionType
+    {
+        SalesInvoice = 1,      // Satış Faturası
+        PurchaseInvoice = 2,   // Alış Faturası
+        Collection = 3,        // Tahsilat (Müşteriden bize)
+        Payment = 4,           // Ödeme (Bizden tedarikçiye)
+        CashEntry = 5,         // Kasaya nakit girişi
+        CashExit = 6,          // Kasadan nakit çıkışı
+        BankTransfer = 7,      // Banka transferi
+        OpeningBalance = 8     // Devir / Açılış Fişi
+    }
+}
